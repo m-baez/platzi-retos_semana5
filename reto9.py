a90142552697cfc15f090a4ae8280049a20f49c6
Marco Baez <mbr.personal@hotmail.com>
2020-07-30 14:33:52 -0500

    challenge 9

--- reto9.py
+++ reto9.py
@@ -1,3 +1,13 @@
 # Reto #9 - Recta numérica
 
 """Escribe un programa donde preguntes a tu usuario si desea una recta numérica positiva o negativa, después pide un número que servirá como límite e imprime todos los números de uno en uno partiendo desde el cero."""
+
+user = str(input('¿Recta numérica positiva o negativa? (p/n) ').lower())
+numb = int(input('Escribe un número que servirá como límite: '))
+if user == 'p':
+    for i in range(numb+1):
+        print(i)
+elif user == 'n':
+    for i in range(numb+1):
+        numb = numb - 1
+        print(-1*i)
